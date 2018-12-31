ab568a4b0825304cb5ea0121b6f58ca29e8fdfa0
johnnycashash <jAG@N1993>
2019-01-01 02:01:48 +0530

    Zuul proxy as Eureka client

--- finance-zuul-proxy/src/main/java/org/ecnanif/zuul/FinanceZuulProxyApplication.java
+++ finance-zuul-proxy/src/main/java/org/ecnanif/zuul/FinanceZuulProxyApplication.java
@@ -2,9 +2,11 @@ package org.ecnanif.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class FinanceZuulProxyApplication {
 
